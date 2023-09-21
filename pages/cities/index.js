1eae3da8aab5eac9fdf8cd184636feb848499e3f
Mukaddes Uzdoganlar <m_uzdoganlar@web.de>
2023-09-21 16:30:19 +0200

    solution

---
+++ pages/cities/index.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import { cities } from "@/lib/data";
+
+export default function Cities() {
+  return (
+    <>
+      <h1>Cities</h1>
+      <ul>
+        {cities.map((city) => (
+          <li key={city.id}>
+            <Link href={`cities/${city.slug}`}>{city.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Welcome to my cities App.</h1>
+      <Link href="/cities">Go to cities</Link>
     </div>
   );
 }
